f2e11a5c2518e33fb87816ba57e528bdb7815f8e
Josef <98840857+Botcoinista@users.noreply.github.com>
2022-10-06 15:33:05 +0200

    Första uppladdningen

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.querySelector("#regform");
+
+const validateText = (id) => {
+    const input = document.querySelector(id)
+}
+
+
+
+form.addEventListener ("submit" e => {
+    e.preventDefault()
+})
